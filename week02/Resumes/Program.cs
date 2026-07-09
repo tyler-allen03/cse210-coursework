b5d1977d9492e74d670b4da17f2f790d98d4a8a7
tyler-allen03 <tylerjallen57@gmail.com>
2026-07-08 20:56:26 -0500

    Finished the Learning Activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.WriteLine("");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2025;
+        job1._endYear = 2026;
+
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Janitor";
+        job2._startYear = 2025;
+        job2._endYear = 2026;
+
+        job2.DisplayJobDetails();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Tyler";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayDetails();
     }
 }
